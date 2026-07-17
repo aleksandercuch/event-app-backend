a335175c8fdb2d401f79d39af60d6e4d628e6a56
unknown <cuch.aleksander@gmail.com>
2026-07-17 10:39:34 +0200

    EAB-1 auth module

---
+++ src/users/user.entity.ts
@@ -0,0 +1,27 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+export enum UserRole {
+  FENCER = 'fencer',
+  ORGANIZER = 'organizer',
+}
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id!: string;
+
+  @Column({ unique: true })
+  email!: string;
+
+  @Column()
+  passwordHash!: string;
+
+  @Column()
+  firstName!: string;
+
+  @Column()
+  lastName!: string;
+
+  @Column({ type: 'enum', enum: UserRole, default: UserRole.FENCER })
+  role!: UserRole;
+}
